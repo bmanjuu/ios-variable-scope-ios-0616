4b390f6dc8c95b6cc11565f7d9c9bcce42188d70
bmanjuu <bettyofung@gmail.com>
2016-06-01 16:29:10 -0400

    second pass at variable scope lab

--- ios-variable-scope/FISAppDelegate.m
+++ ios-variable-scope/FISAppDelegate.m
@@ -19,10 +19,33 @@
     return YES;
 }
 
-/*
- 
- * Write your method definitions here.
- 
- */
+-(NSMutableArray *)arrayByAddingString:(NSString *)string toArray:(NSMutableArray *)array{
+    
+    NSMutableArray *mArray = [array mutableCopy];
+    [mArray addObject:string];
+    
+    return mArray;
+}
+
+
+
+-(NSUInteger)countOfUppercaseStringsInArray:(NSArray *)array{
+    
+    NSUInteger count = 0;
+    
+    for(NSUInteger i = 0; i < [array count]; i++){
+        if ([array[i] isEqualToString:[array[i] uppercaseString]]){
+            count++;
+        }
+    }
+    
+    return count;
+}
+
+
+
+-(void)removeAllObjectsFromArray:(NSMutableArray *)array{
+    [array removeAllObjects];
+}
 
 @end
